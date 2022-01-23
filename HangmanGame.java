30bc8ee070e1670a66c70ec2c3c1787808e2d3b1
malcolmbly <malcolmhbailey@gmail.com>
2022-01-22 17:57:14 -0700

    Added skeleton methods

---
+++ HangmanGame.java
@@ -0,0 +1,42 @@
+package a01;
+
+public class HangmanGame {
+	private int totalLives = 6;
+	private int currentLives = 6;
+	private String secretWord = "Panoply";
+	
+	
+	private void guessLetter() {
+		
+	}
+	
+	public String showGuesses() {
+		
+	}
+	
+	public int getLives() {
+		return this.currentLives;
+	}
+	
+	private void loseLife() {
+		
+	}
+	
+	private boolean gameWon() {
+		
+	}
+	
+	private boolean gameLost() {
+		return this.currentLives == 0;
+	}
+	
+	private String gameOverScreen() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
